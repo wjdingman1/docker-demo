3078dda31aad24a91bfe976bffe80b2b4c2ab460
Wes Dingman <wdingman@asymmetrik.com>
2021-11-11 10:01:43 -0500

    Add main functionality

--- client/client.js
+++ client/client.js
@@ -1,1 +1,30 @@
+const axios = require("axios");
 
+const host = process.env.HOST || "localhost";
+const port = process.env.POST || 3000;
+const url = `http://${host}:${port}`;
+
+const route = process.argv[2];
+
+switch (route) {
+  case "1":
+    console.log(`Making request to ${url}/one`);
+    axios.get(`${url}/one`).then((res) => {
+      console.log(res.data);
+    });
+    break;
+
+  case "2":
+    console.log(`Making request to ${url}/two`);
+    axios.get(`${url}/two`).then((res) => {
+      console.log(res.data);
+    });
+    break;
+
+  case "3":
+    console.log(`Making request to ${url}/three`);
+    axios.get(`${url}/three`).then((res) => {
+      console.log(res.data);
+    });
+    break;
+}
---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const http = require("http");
+
+const app = express();
+
+// Create three simple GET routes
+
+app.use("/one", (req, res) => {
+  console.log("Incoming request from client at route 1");
+  res.send("Response from route 1");
+});
+
+app.use("/two", (req, res) => {
+  console.log("Incoming request from client at route 2");
+  res.send("Response from route 2");
+});
+
+app.use("/three", (req, res) => {
+  console.log("Incoming request from client at route 3");
+  res.send("Response from route 3");
+});
+
+http.createServer(app);
+
+app.listen(3000);
